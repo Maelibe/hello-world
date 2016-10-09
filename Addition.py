b57926a7001fe6dfdce211550c9aef38e712b272
Maelibe <amarylisa000@gmail.com>
2016-10-09 15:34:55 +0200

    Simple addition

--- Addition.py
+++ Addition.py
@@ -1,6 +1,6 @@
 print ("Give me a number ")
-num1 = int(input(" "))
+num1 = int(input("  "))
 print ("Give me a second number ")
-num2 = int(input(" "))
+num2 = int(input("  "))
 product = num1 + num2
 print ("Sum of those numbers is: ", product)
