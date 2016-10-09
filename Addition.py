81323b36c8021281f554cd93a9dacdaab1872cac
Maelibe <amarylisa000@gmail.com>
2016-10-09 15:51:56 +0200

    readding file

---
+++ Addition.py
@@ -0,0 +1,7 @@
+print ("Give me a number ")
+num1 = int(input("  "))
+print ("Give me a second number ")
+num2 = int(input("  "))
+product = num1 + num2
+print ("Sum of those numbers is: ", product)
+print ("This is commit from Linux side")
